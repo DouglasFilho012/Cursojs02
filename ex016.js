a2c04e515730bbf875bbbeb79372ed3ab6e35e5f
Douglas Filho <109813514+DouglasFilho012@users.noreply.github.com>
2022-09-20 12:46:12 -0300

    Update ex016.js

--- ex016.js
+++ ex016.js
@@ -5,14 +5,28 @@ function Contar() {
     var resultado = document.getElementById('res')
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         window.alert('[ERRO] FALTAM DAODS!')
+        resultado.innerHTML = 'Impossivel contar!'
     } else {
-        resultado.innerHTML = `Contando: `
+        resultado.innerHTML = `Contando: <br> `
         var i = Number(ini.value)
         var f = Number(fim.value)
         var p = Number(passo.value)
-
-        for (var c = i; c <= f; c += p) {
-            resultado.innerHTML += `${c}`
+        if (p <= 0) {
+            window.alert ('Passo invalido! Considerando PASSO 1')
+            p= 1
         }
+        if (i < f) {
+        for (var c = i; c <= f; c += p) {
+            // contagem crescente
+            resultado.innerHTML += `${c} \u{1F449}`
+        }
+        
+    } else {
+        for (var c = i; c>=f ; c-= p) {
+            // regresiva
+            resultado.innerHTML += `${c} \u{1F449}`
+        } 
+    } 
+    resultado.innerHTML += `\u{1F3C1} `
     }
 }
